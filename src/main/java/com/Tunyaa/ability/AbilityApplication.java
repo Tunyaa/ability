80311d5ed71044370ea9c9c86c6f98484741da03
Tunyaa <oldcampthunder@gmail.com>
2024-04-18 18:41:19 +0300

    empty project

---
+++ src/main/java/com/Tunyaa/ability/AbilityApplication.java
@@ -0,0 +1,13 @@
+package com.Tunyaa.ability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbilityApplication.class, args);
+	}
+
+}
